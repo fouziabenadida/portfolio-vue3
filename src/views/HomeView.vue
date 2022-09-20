8ece2c56a7ec36c70f18a03f79e35e7d475c4c8c
benadidafouzia <benadidafouzia@gmail.com>
2022-09-20 16:12:57 +0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+<div></div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+    
+  },
+});
+</script>

1a9950fe7a39aa37d41b3e123bfa32c8cfc97be6
benadidafouzia <benadidafouzia@gmail.com>
2022-09-20 16:53:32 +0300

    AppBar commit

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,19 +1,23 @@
+// Styles
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import '@mdi/font/css/materialdesignicons.css'
 
-Vue.use(Vuetify);
+import 'vuetify/styles'
 
-export default new Vuetify({
-    icons: {
-        iconfont: 'fa' || 'md' || 'mdi'
-    },
-    theme: {
-        themes: {
-            dark: {
-                background: '#111111'
-            }
-        }
-    }
+// Vuetify
+import { createVuetify } from 'vuetify'
+
+export default createVuetify({
+  icons: {
+      iconfont: 'fa' || 'md' || 'mdi'
+  },
+  theme: {
+      themes: {
+          dark: {
+              background: '#111111'
+          }
+      }
+  }
 });
+ 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,104 @@
 <template>
-<div></div>
+  <v-app >
+    <Navbar/>
+  </v-app>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import Navbar from '../components/Navbar/Navbar.vue'
 
-// Components
-
-
-export default defineComponent({
-  name: 'HomeView',
+export default {
+  name: "HomeView",
 
   components: {
+    Navbar,
     
   },
-});
+};
 </script>
+<style scoped>
+.top {
+  margin-top: 180px;
+}
+.topInverse {
+  margin-top: -250px;
+}
+.topTolbar {
+  margin-top: 100px;
+  text-align: center;
+}
+.first {
+  width: 100%;
+  height: 610px;
+  background: linear-gradient(
+    to right,
+    #181818,
+    #181818 50%,
+    #111111 50%,
+    #111111 50%
+  );
+  text-align: center;
+  padding: 2rem 2rem;
+}
+.second {
+  width: 100%;
+  height: 400px;
+  background: #181818;
+  text-align: center;
+  padding: 2rem 2rem;
+}
+.secondchild1 {
+  display: inline-block;
+  background-color: #1e1e1e;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  text-align: left;
+  margin-top: 250px;
+}
+.child {
+  display: inline-block;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  text-align: left;
+  margin-right: 8px;
+}
+.bgColor1 {
+  background-color: #1e1e1e;
+}
+.bgColor2 {
+  background-color: #ce1d2a;
+}
+
+.child1 {
+  display: inline-block;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  margin-right: 5px;
+  width: 240px;
+}
+.child2 {
+  display: inline-block;
+  width: 245px;
+  vertical-align: middle;
+}
+.mRight {
+  margin-right: 8px;
+}
+.mButton {
+  margin-bottom: 8px;
+}
+.padding {
+  padding: 8px 0;
+}
+
+.col-12.padd {
+  padding: 12px 0 !important;
+}
+.col-12.childcol {
+  padding: 0 !important;
+}
+h1.number {
+  font-size: 50px;
+  font-weight: bold;
+}
+</style>
